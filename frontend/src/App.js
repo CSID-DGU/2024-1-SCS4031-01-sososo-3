bbc989c684068d5905551d8383a4cb885063e22f
MeMe4834 <2021110472@dgu.ac.kr>
2024-04-29 17:25:19 +0900

    [KMS]del_결재대기칸

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import './App.css';
 import EmployeeHeader from './pages/EmployeeHeader';
-import Approval from './pages/Approval';
 import TaskList from './pages/TaskList'
 import DateComponent from './pages/DateComponent'
 
@@ -12,12 +11,7 @@ const App = () => {
     <div className="app">
       <EmployeeHeader />
       <div className="rounded-rectangles-container">
-        <div className="rounded-rectangle-wrapper">
-          <Approval />
-        </div>
-        <div className="rounded-rectangle-wrapper">
           <DateComponent />
-        </div>
       </div>
       <div className="task-list-container">
         <TaskList />
