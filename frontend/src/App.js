a850c28a2f13c4a2578a3388d12bb5fe3d07c395
MeMe4834 <2021110472@dgu.ac.kr>
2024-05-01 15:31:13 +0900

    [KMS]add_팀페이지라우팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main/MainPage'; 
+import TeamPage from './pages/team/TeamPage'
 import './App.css';
+
 
 const App = () => {
   return (
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          {/* 나머지 라우팅 설정 */}
+          <Route path="/" element={<MainPage />} />{/* MainPage 라우팅 추가 */}
+          <Route path="/team" element={<TeamPage />} /> {/* TeamPage 라우팅 추가 */}
         </Routes>
       </div>
     </Router>
