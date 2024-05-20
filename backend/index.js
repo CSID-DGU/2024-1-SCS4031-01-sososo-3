6c48c1076a942e367674bc1ec331413cacc322e6
MeMe4834 <2021110472@dgu.ac.kr>
2024-05-20 12:07:45 +0900

    [KMS]fix_DBrouting/ADD_로그인기능

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,37 @@
 const express = require('express'); // express 임포트
-const app = express(); // app생성
-const port = 3000;
+const app = express(); // app 생성
+const cors = require("cors")
+const UserModel = require("./models/user");
+//const MemberModel = require("./models/member");
+// const GroupModel = require("./models/group");
+// const LebelModel = require("./models/level");
+// const RoomModel = require("./models/room");
+// const StateModel = require("./models/state");
+// const TaskModel = require("./models/task");
 
-app.use(express.json()); //JSON
+const port = 3001;
 
-app.get('/', function (req, res) {
-  res.send('hello world!!');
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Backend Server is running');
 });
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
 
-const memberRouter = require('./routes/member'); //라우터 가져오기
 
-app.use('/api', memberRouter); //라우터 등록
+//라우팅
+//const memberRouter = require('./routes/member'); //member라우터 가져오기
+//const userRouter = require('./routes/user'); //user라우터 가져오기
 
-app.listen(port, () => console.log(`${port}포트입니다.`));
+//app.use('/api', memberRouter); //member라우터 등록
+//app.use('/api', userRouter); //user라우터 등록
 
 //몽구스 연결
 const mongoose = require('mongoose');
 mongoose
   .connect(
-    "mongodb+srv://ottffss1005:Wooang6324@cluster0.cgrpj1p.mongodb.net/",
-    
+    "mongodb+srv://ottffss1005:Wooang6324@cluster0.cgrpj1p.mongodb.net/sososo",
     {
       // useNewUrlPaser: true,
       // useUnifiedTofology: true,
@@ -29,7 +41,23 @@ mongoose
   )
   .then(() => console.log('MongoDB conected'))
   .catch((err) => {
-    console.log("오류입니다");
+    console.log("connected error");
     console.log(err);
   });
 
+  // 로그인
+  app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    UserModel.findOne({email : email})
+    .then(user => {
+        if(user) {
+            if(user.password === password){
+                res.json("Success")
+            }else{
+                res.json("The password is incorrect")
+            }
+        }else{
+            res.json("No record existed")
+        }
+    })
+})
