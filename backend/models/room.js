a560b21ef3f05ad1c5638bbc8efbc2212a7e2889
MeMe4834 <2021110472@dgu.ac.kr>
2024-05-20 12:22:08 +0900

    [KMS]fix_dbname

---
+++ backend/models/room.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+
+  roomId: { 
+    type: String, 
+    required: true,
+    unique:1
+    },
+  businessNum: {type: String}, //사업 번호
+  groupCode: { 
+        type: String,
+        unique:1
+    }, //조직코드
+  roomName: {
+    type: String,
+    maxlength:50
+    }, //공간명
+  parentRoom_id: {type: String}, //상위공간
+  groupLevel: {type: Number}, //조직레벨
+  ownerBusinessNumber: {type: String} //공간소유사업번호
+
+});
+
+module.exports = mongoose.model('Room', roomSchema);
