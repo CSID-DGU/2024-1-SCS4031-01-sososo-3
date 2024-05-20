09778b4bfc608aad890877614aaab71c447d8e21
MeMe4834 <2021110472@dgu.ac.kr>
2024-05-20 16:09:36 +0900

    [KMS]add_로그인라우팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import MainPage from './pages/main/MainPage'; 
-import TeamPage from './pages/team/TeamPage'
-import DivisionPage from './pages/division/DivisionPage'
-import DirectorPage from './pages/director/DirectorPage'
+import TeamPage from './pages/team/TeamPage';
+import DivisionPage from './pages/division/DivisionPage';
+import DirectorPage from './pages/director/DirectorPage';
+import Login from './pages/Login';
 import './App.css';
 
 
 const App = () => {
   return (
     <Router>
-      <div className="app">
+      <div>
         <Routes>
-          <Route path="/" element={<MainPage />} />{/* MainPage 라우팅 추가 */}
+          <Route path="/" element={<Navigate to="/login" />} /> {/* 기본 경로로 접근 시 login 페이지로 리디렉션 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/main" element={<MainPage />} />{/* MainPage 라우팅 추가 */}
           <Route path="/team" element={<TeamPage />} /> {/* TeamPage 라우팅 추가 */}
           <Route path="/division" element={<DivisionPage />} /> {/* DivisionPage 라우팅 추가 */}
           <Route path="/director" element={<DirectorPage />} /> {/* DirectorPage 라우팅 추가 */}
-
           <Route path="/director/4" element={<DivisionPage />} /> {/* 시스템영업본부 라우팅 추가*/}
           <Route path="/division/2" element={<TeamPage />} /> {/* 시스템영업1팀 라우팅 추가*/}
           <Route path="/team/:id" element={<MainPage />} /> {/* 사원페이지 라우팅 추가*/}
