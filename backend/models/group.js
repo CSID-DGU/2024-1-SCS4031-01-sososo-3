cb47ac9ff8fe9d1028f3e5d60e3017c7e7629c5a
MeMe4834 <2021110472@dgu.ac.kr>
2024-05-26 17:58:33 +0900

    [KMS]fix_group스키마

--- backend/models/group.js
+++ backend/models/group.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
   groupCode: { type: String, 
-    // required: true, unique:1
+  required: true, 
+  unique:1
   },
   groupName: { type: String },
-  groupLevel: { type: String },
-  parentGroupCode: { type: String },
-  leaderEmployeeNumber: { type: String }
+  groupLevel: { type: String }, // 팀:1, 본부:2, 대표이사:3
+  parentGroupCode: { type: String }, // 상위조직 groupCode
+  leaderRoomId: { type: String } // 리더 roomId 
 });
 
 module.exports = mongoose.model('Group', groupSchema);
