31b150ad72613594b407e8d0262eeefb7551a2e4
MeMe4834 <2021110472@dgu.ac.kr>
2024-06-06 02:34:24 +0900

    [KMS]fix_테스크라우팅

--- frontend/src/pages/main/MainPage.js
+++ frontend/src/pages/main/MainPage.js
@@ -1,5 +1,6 @@
 // MainPage.js
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import '../../App.css';
 import EmployeeHeader from './EmployeeHeader';
 import TaskList from './TaskList';
@@ -7,12 +8,13 @@ import DateComponent from './DateComponent';
 
 const MainPage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const { roomId } = useParams(); // Get the roomId from the URL
   
   return (
     <div>
       <EmployeeHeader />
       <DateComponent onDateChange={setSelectedDate} />
-      <TaskList selectedDate={selectedDate} />
+      <TaskList selectedDate={selectedDate} roomId={roomId} />
     </div>
   );
 }
